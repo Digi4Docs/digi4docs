9b501428d4b4b772c8ff9a89bd88051f8e69aedd
susanna.kunzmann@gmail.com <susanna.kunzmann@gmail.com>
2022-03-25 18:16:17 +0100

    Add task to dos to home

--- src/main/java/de/digidoc/controller/HomeController.java
+++ src/main/java/de/digidoc/controller/HomeController.java
@@ -2,19 +2,19 @@ package de.digidoc.controller;
 
 import de.digidoc.model.Course;
 import de.digidoc.model.Role;
+import de.digidoc.model.User;
+import de.digidoc.model.UserTask;
 import de.digidoc.service.CourseService;
 import de.digidoc.service.UserService;
+import de.digidoc.service.UserTaskService;
 import de.digidoc.util.ProgressCountProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -25,19 +25,29 @@ public class HomeController {
     private UserService userService;
 
     @Autowired
+    private UserTaskService userTaskService;
+
+    @Autowired
     private ProgressCountProvider progressCountProvider;
 
     @GetMapping(value = {"/home", "/"})
     public String home(Model model) {
-        model.addAttribute("currentUser", userService.findCurrentUser());
+        User currentUser = userService.findCurrentUser();
+        model.addAttribute("currentUser", currentUser);
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        boolean isStudent = auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()).contains(Role.STUDENT.toString());
+        boolean isStudent = userService.hasUserRole(currentUser, Role.STUDENT);
         if (isStudent) {
             model.addAttribute("courses", courseService.findAllActive());
             Map<Course, Integer> coursePersonTaskCount = progressCountProvider.getPersonalCourseTaskCountMap();
             model.addAttribute("personalCourses", coursePersonTaskCount);
             model.addAttribute("courseTaskCounts", progressCountProvider.getGeneralCourseTaskCountMap(coursePersonTaskCount.keySet()));
+        }
+
+        boolean showTeacherTasks = userService.hasUserRole(currentUser, Role.ADMIN) || userService.hasUserRole(currentUser, Role.TEACHER);
+        model.addAttribute("showTeacherTasks", showTeacherTasks);
+        if (showTeacherTasks) {
+            List<UserTask> teacherTasks = userTaskService.findTransmittedOfCurrentUser();
+            model.addAttribute("teacherTasks", teacherTasks);
         }
 
         return "home";
